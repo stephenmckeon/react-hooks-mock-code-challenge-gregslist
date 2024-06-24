20b8b7db37d61100dca4608870f3c75937a4d9bc
Stephen McKeon <stephen.mckeon@powerhrg.com>
2024-06-24 13:26:47 -0400

    Toggle start on click

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,18 +1,22 @@
-import React from "react"
+import React, { useState } from "react"
 
 function ListingCard({ listing: { image, description, location } }) {
+  const [isFavorite, setIsFavorite] = useState(true)
+
+  const handleClick = () => setIsFavorite(!isFavorite)
+
   return (
     <li className="card">
       <div className="image">
         <span className="price">$0</span>
         <img src={image} alt={description} />
       </div>
-      <div className="details">
-        {true ? (
-          <button className="emoji-button favorite active">★</button>
-        ) : (
-          <button className="emoji-button favorite">☆</button>
-        )}
+      <div className="details" onClick={handleClick}>
+        <button
+          className={`emoji-button favorite ${isFavorite ? "active" : ""}`}
+        >
+          {isFavorite ? "★" : "☆"}
+        </button>
         <strong>{description}</strong>
         <span> · {location}</span>
         <button className="emoji-button delete">🗑</button>
